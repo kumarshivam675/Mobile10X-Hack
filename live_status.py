632fff47bbafa86f58a8d4baf2ad80e865888426
sammme <samyak.upadhyay123@gmail.com>
2016-02-13 20:38:24 +0530

    changed live_status file formating

--- live_status.py
+++ live_status.py
@@ -3,8 +3,9 @@ import json
 import sys
 
 #pnr = 4627941857
-def waypoints(train, date_val):
-    #base = http://api.railwayapi.com/live/train/22351/doj/20160212/apikey/icoyi9743/
+def live_status(train, date_val):
+    #base = http://api.railwayapi.com/live/train/22351/doj/20160212/apikey/yourapikey/
+    ans = ""
     base = "http://api.railwayapi.com/live/train/"
     pnr_val = str(train)
     date = "/doj/" + str(date_val)
@@ -12,15 +13,20 @@ def waypoints(train, date_val):
     url = base+pnr_val+date+apikey
     response = urllib2.urlopen(url)
     data = json.load(response)
-    print url
+    #print url
     #print data["position"].split(" ")[-2]
     location = data["position"]
     time = data["position"].split(" ")[-2]
     #print location
     value = divmod(int(time), 60)
-    print location
-    print value
-    print str(value[0])+" hrs"+" and "+str(value[1])+" mins"
+    #ans += location
+    #print location
+    ans += location + "\n"
+    #print value
+    in_hrs = str(value[0])+" hrs"+" and "+str(value[1])+" mins"
+    ans += in_hrs
+    print ans
+    return ans
     #print data["position"].split(" ")[-2]
 
-#waypoints(sys.argv[1], sys.argv[2])
+live_status(sys.argv[1], sys.argv[2])
